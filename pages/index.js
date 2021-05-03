88a4055ad3b27a29b7814397f9581ca34c333c3e
Punit Mishra <punit.mishra09@gmail.com>
2021-05-02 23:26:25 -0700

    Adding troll pages

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,44 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Hello World!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to :trollminds :D 
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          For trolls by trolls, with love!{' '}
+          <code className={styles.code}>hack</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://giphy.com/gifs/cat-dancing-dance-M5T9ySMRsE98c" className={styles.card} >
+          <iframe src="https://giphy.com/embed/M5T9ySMRsE98c"  width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen className={styles.card} ></iframe>
+            <h3>Cats &#x1F419;</h3>
+            <p>Step 1. Find a cat troll video.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://giphy.com/gifs/fly-ufo-spaceship-26BoCVdjSJOWT0Fpu" className={styles.card}>
+          <iframe src="https://giphy.com/embed/26BoCVdjSJOWT0Fpu" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen className={styles.card} ></iframe>
+            <h3>Aliens &#x1F991;</h3>
+            <p>You know what's happening!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://giphy.com/gifs/alien-cow-flying-saucer-ZEl0gSSnbEzJ7qr18E" className={styles.card} >
+            <iframe src="https://giphy.com/embed/ZEl0gSSnbEzJ7qr18E" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen className={styles.card} ></iframe>
+            <h3>The Future &#x1F42E;</h3>
+            <p>When the cow gains power!!</p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+          <a href="https://giphy.com/gifs/pbskids-happy-space-8lNaZuf2ZmF54oalhP" className={styles.card}>
+          <iframe src="https://giphy.com/embed/8lNaZuf2ZmF54oalhP" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen className={styles.card} ></iframe>
+            <h3>Happy Space !!</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              The End :D
             </p>
           </a>
         </div>
